3420cb929a2e195b4e2f30d09a60b769a19f25d4
Matthew J. Whitney <matthewjwhitney@gmail.com>
2020-02-23 19:16:32 -0700

    update packages and use BrowserRouter instead of Router with history

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
-/*!
-
-=========================================================
-* Material Dashboard PRO React - v1.8.0
-=========================================================
-
-* Product Page: https://www.matthewjwhitney.com/product/material-dashboard-pro-react
-* Copyright 2019 Matthew Whitney (https://www.matthewjwhitney.com)
-
-* Coded by Matthew Whitney
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import AuthLayout from "layouts/Auth.js";
 import RtlLayout from "layouts/RTL.js";
@@ -25,16 +8,14 @@ import AdminLayout from "layouts/Admin.js";
 
 import "assets/scss/material-dashboard-pro-react.scss";
 
-const hist = createBrowserHistory();
-
 ReactDOM.render(
-  <Router history={hist}>
+  <BrowserRouter>
     <Switch>
       <Route path="/rtl" component={RtlLayout} />
       <Route path="/auth" component={AuthLayout} />
       <Route path="/admin" component={AdminLayout} />
       <Redirect from="/" to="/admin/dashboard" />
     </Switch>
-  </Router>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
